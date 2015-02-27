0752bedf7756bb793a23a84e6b5459dda6a9d323
Moustafa Saleh <msaleh83@gmail.com>
2015-02-27 16:18:14 -0600

    Added function ReplaceAll

--- Util.h
+++ Util.h
@@ -66,6 +66,15 @@ static inline std::string &trim(std::string &s) {
         return ltrim(rtrim(s));
 }
 
+static inline std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
+	size_t start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+	}
+	return str;
+}
+
 template <typename T>
 string numToStr(T number)
 {
